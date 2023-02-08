a9c5b42ef69bede77d200d0924c17837bbf9e45c
Ahmed Karam <ahmedkarm749@gmail.com>
2023-02-08 17:51:30 +0200

    edit course grades

--- src/app/common/course-grades/course-grades.component.ts
+++ src/app/common/course-grades/course-grades.component.ts
@@ -1,82 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ViewChild } from '@angular/core';
 import { studData } from './stud-data';
 import { GradeService } from './grade.service';
 
-
 @Component({
   selector: 'app-course-grades',
   templateUrl: './course-grades.component.html',
-  styleUrls: ['./course-grades.component.css']
+  styleUrls: ['./course-grades.component.css'],
 })
-  
-  
 export class CourseGradesComponent implements OnInit {
-  @ViewChild('termWorkInput') termWorkInput: any;
-  @ViewChild('examWorkInput') examWorkInput: any;
-  
-  
+  @ViewChild('termWork') termWorkInput: ElementRef;
+  @ViewChild('examWork') examWorkInput: ElementRef;
+
   students = studData.map((student) => {
     return {
       ...student,
-      editable: false
-    }
+      editable: false,
+    };
   });
-  editable = false;
-  
 
-  isValidated = false;
-  constructor(private gradeService: GradeService) { }
+  constructor(private gradeService: GradeService) {}
 
-    // validate examwork and termwork inputs to be less than 60 and 40 respectively
-  
-  updateExamWork(index: number, event: any) {
-    this.students[index].examWork = event.target.value;
-  }
-
-  updateTermWork(index: number, event: any) {
-    this.students[index].termWork = event.target.value;
-  }
-
-  
-
- 
-
-  edit(index: number) {
+  save(index: number, termWork: number, examWork: number) {
     this.students[index].editable = !this.students[index].editable;
-    // this.students[index].grade = this.gradeService.calculateGrade(this.students[index].total);
-    if (this.students[index].termWork !==null && this.students[index].examWork !== null) {
-      this.students[index].total = +this.students[index].termWork + +this.students[index].examWork;
-      this.calculateGrade(index);      
-    }
-
+    this.students[index].termWork = termWork;
+    this.students[index].examWork = examWork;
+    this.students[index].total = +termWork + +examWork;
+    this.calculateGrade(index);
   }
-  
-  // make cancel function
+
   cancel(index: number) {
     this.students[index].editable = !this.students[index].editable;
-    // this.students[index].termWork = null;
-    // this.students[index].examWork = null;
-    // this.students[index].total = null;
-    // this.students[index].grade = null;
-    console.log(this.termWorkInput);
-    
-    this.termWorkInput.control.value = this.students[index].termWork;
-    this.examWorkInput.control.value = this.students[index].examWork;
-    console.log(this.termWorkInput.viewModel);
-    console.log(this.students[index].termWork);
+    this.termWorkInput.nativeElement.value = this.students[index].termWork;
+    this.examWorkInput.nativeElement.value = this.students[index].examWork;
+    console.log(this.termWorkInput.nativeElement.value);
+    console.log(this.examWorkInput.nativeElement.value);
   }
 
-  
   calculateGrade(index: number) {
-    this.students[index].grade = this.gradeService.calculateGrade(this.students[index].total);
-  }
-  
-  
-  ngOnInit(): void {
-    
-    
+    this.students[index].grade = this.gradeService.calculateGrade(
+      this.students[index].total
+    );
   }
 
+  ngOnInit(): void {}
 }
