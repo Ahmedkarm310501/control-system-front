be42ee8072d0bf36cafb35cc858220043bc3d531
youssef salah <youssefsalahcs@gmail.com>
2023-02-08 15:28:13 +0200

    make adminlog page

--- src/app/admin/pages/admin-log/admin-log.component.ts
+++ src/app/admin/pages/admin-log/admin-log.component.ts
@@ -11,5 +11,10 @@ export class AdminLogComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  dummyData = [
+    { id: 1, username: 'john doe', change: 'add grade for course IS111', Time: '2021-01-01 12:00:00' },
+    { id: 2, username: 'Jane Doe', change: 'add grade for course CS111', Time: '2021-01-02 1:10:20' },
+    { id: 3, username: 'Bob Smith', change: 'add grade for course DS191', Time: '2021-01-03 2:230:00' },
+    { id: 4, username: 'youssef', change: 'add grade for course AI811', Time: '2021-01-01 12:00:00' },
+  ];
 }
