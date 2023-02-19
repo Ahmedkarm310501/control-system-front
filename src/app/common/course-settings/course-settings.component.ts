4dc78115d04f9c0550f33b769802d785748d81b9
abdogamal90 <abdogamalrahim@gmail.com>
2023-02-19 19:44:04 +0200

    unified all forms styles, added links to sidebar

--- src/app/common/course-settings/course-settings.component.ts
+++ src/app/common/course-settings/course-settings.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+
 import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-course-settings',
   templateUrl: './course-settings.component.html',
-  styleUrls: ['./course-settings.component.css']
+  styleUrls: ['./course-settings.component.css'],
 })
 export class CourseSettingsComponent implements OnInit {
-
-  // Form Controls
-  
   onSubmit(form: any) {
     console.log(form);
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  courseID = 'IS123';
+  courseNamee = 'Intro to Database Systems';
+  termWorkk = 40;
+  examWorkk = 60;
+  departmentt = 'IS';
+  instructorr = 'Ali Zidane';
+  creditss = 3;
+  raafaa = 2;
 }
