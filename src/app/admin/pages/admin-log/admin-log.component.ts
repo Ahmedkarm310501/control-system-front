9f2a2cc47a86f430ebca062ae5d42883aea8b278
mohamed-gamal-20190427 <20190427@stud.fci-cu.edu.eg>
2023-06-16 18:35:54 +0300

    add before and after change in log

--- src/app/admin/pages/admin-log/admin-log.component.ts
+++ src/app/admin/pages/admin-log/admin-log.component.ts
@@ -7,42 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminLogComponent implements OnInit {
   constructor() {}
+  collapseStates: { [key: number]: boolean } = {};
 
-  ngOnInit(): void {}
   dummyData = [
     {
       id: 1,
       username: 'Dr Ali Zidan',
       change: 'add grade for course IS111',
       Time: '2022-02-06 12:04:33',
+      Type: 'add',
+      before: 'null',
+      after: 'upload students grades',
     },
     {
       id: 2,
       username: 'Dr Iman Helal',
       change: 'add grade for course CS111',
       Time: '2022-02-06 11:10:20',
+      Type: 'add',
+      before: 'add students names',
+      after: 'update students names',
     },
     {
       id: 3,
       username: 'Dr Ayman El-Kilany',
       change: 'add grade for course IS191',
       Time: '2022-02-04 2:16:35',
+      type: 'show',
+      before: 'null',
+      after: 'update some grades',
     },
     {
       id: 4,
       username: 'Dr Mohamed Nour',
       change: 'add grade for course IS811',
       Time: '2022-02-03 04:00:44',
+      Type: 'add',
+      before: 'grades',
+      after: 'update some grades',
     },
     {
       id: 5,
       username: 'Dr Osama Ismail',
       change: 'add grade for course IS211',
       Time: '2022-02-02 12:10:00',
+      Type: 'show',
+      before: 'null',
+      after: 'upload students names',
     },
   ];
   filteredCourses = this.dummyData;
-
+  ngOnInit(): void {
+    this.dummyData.forEach((data) => {
+      this.collapseStates[data.id] = true;
+    });
+  }
   searchCourse(searchTerm: string) {
     this.filteredCourses = this.dummyData.filter((log) => {
       return (
@@ -51,4 +70,13 @@ export class AdminLogComponent implements OnInit {
       );
     });
   }
+
+  toggleCollapse(id: number) {
+    this.collapseStates[id] = !this.collapseStates[id];
+  }
+
+  isCollapsed(id: number) {
+    return this.collapseStates[id];
+  }
+  downloadFile() {}
 }
