2dd0513966eddb1c8a49f1d91ff30dc9400e35cb
mohamed-gamal-20190427 <20190427@stud.fci-cu.edu.eg>
2023-06-18 02:13:06 +0300

    fetch submit and fetch course dept

--- src/app/common/course-settings/course-settings.component.ts
+++ src/app/common/course-settings/course-settings.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormControl } from '@angular/forms';
 import { CourseSettingsService } from './course-settings.service';
+import { SnackbarComponent } from 'src/app/components/snackbar/snackbar.component';
 
 @Component({
   selector: 'app-course-settings',
@@ -9,37 +10,73 @@ import { CourseSettingsService } from './course-settings.service';
   styleUrls: ['./course-settings.component.css'],
 })
 export class CourseSettingsComponent implements OnInit {
-  onSubmit(form: any) {
-    console.log(form);
-  }
-
+  @ViewChild('snackbar') snackbar: SnackbarComponent;
+  message: string;
+  type: string;
   constructor(
     private route: ActivatedRoute,
     private courseSettingsService: CourseSettingsService
   ) {}
-  courseId = this.route.snapshot.paramMap.get('courseId');
-  courseName: string;
-  courseCode: string;
-  department: string;
+  course_id = this.route.snapshot.paramMap.get('courseId');
+  semester_id = this.route.snapshot.paramMap.get('termId');
+  course_name: string;
+  course_code: string;
+  dept_code: string = '';
   instructor: string;
-  termWork: number;
-  examWork: number;
-  totalGrade: number;
-
+  term_work: number;
+  exam_work: number;
+  total: number;
+  departments: any = [];
   ngOnInit(): void {
-    this.courseSettingsService.getCourseData(this.courseId).subscribe(
+    this.courseSettingsService.getCourseData(this.course_id).subscribe(
       (data) => {
-        this.courseCode = data.data.courseID;
-        this.courseName = data.data.courseName;
-        this.department = data.data.department;
+        this.course_code = data.data.courseID;
+        this.course_name = data.data.courseName;
+        this.dept_code = data.data.department;
         this.instructor = data.data.instructor;
-        this.termWork = data.data.termWork;
-        this.examWork = data.data.examWork;
-        this.totalGrade = data.data.totalGrade;
+        this.term_work = data.data.termWork;
+        this.exam_work = data.data.examWork;
+        this.total = data.data.totalGrade;
       },
-      (error) => {
-        console.log(error);
+      (err) => {
+        this.message = err.error.message;
+        this.type = 'failed';
+        console.log(err);
+        this.snackbar.show();
       }
     );
+    this.courseSettingsService.getAllDepartments().subscribe((res) => {
+      this.departments = res.data;
+      console.log(this.departments);
+    });
+  }
+  onSubmit(form: any) {
+    console.log(form);
+    this.courseSettingsService
+      .editCourseData(
+        this.course_id,
+        this.semester_id,
+        form.course_code,
+        form.course_name,
+        form.dept_code,
+        form.instructor,
+        form.term_work,
+        form.exam_work,
+        form.total
+      )
+      .subscribe(
+        (res) => {
+          this.message = 'Course Updated Successfully';
+          this.type = 'success';
+          console.log(res);
+          this.snackbar.show();
+        },
+        (err) => {
+          this.message = err.error.message;
+          this.type = 'failed';
+          console.log(err);
+          this.snackbar.show();
+        }
+      );
   }
 }
