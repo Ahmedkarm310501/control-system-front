7da9f4a2482686457cc4edbe5a19958e223d5cf0
mohamed-gamal-20190427 <20190427@stud.fci-cu.edu.eg>
2023-06-22 01:29:33 +0300

    add route to all students

--- src/app/admin/pages/add-user/add-user.component.ts
+++ src/app/admin/pages/add-user/add-user.component.ts
@@ -1,7 +1,7 @@
 import { AddUserService } from './add-user.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { SnackbarComponent } from 'src/app/components/snackbar/snackbar.component';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
@@ -12,7 +12,7 @@ export class AddUserComponent implements OnInit {
   message: string;
   type: string;
   isAdmin: boolean = false;
-  constructor(private addUser: AddUserService) {}
+  constructor(private addUser: AddUserService, private router: Router) {}
 
   ngOnInit(): void {}
   onSubmit(form: any) {
@@ -31,6 +31,9 @@ export class AddUserComponent implements OnInit {
           this.type = 'success';
           console.log(res);
           this.snackbar.show();
+          setTimeout(() => {
+            this.router.navigate(['/all-users']);
+          }, 2000);
         },
         (err) => {
           this.message = err.error.message;
@@ -49,6 +52,5 @@ export class AddUserComponent implements OnInit {
     form.password_confirmation = '';
     form.national_id = '';
     form.is_admin = false;
-
   }
 }
