710f47284a470d7736ff9665c72b7b3bb9245fe2
abdogamal90 <abdogamalrahim@gmail.com>
2023-06-21 18:51:40 +0300

    fixed some bugs in log and fixed some bugs in add course and compare courses

--- src/app/admin/pages/add-course/add-course.component.ts
+++ src/app/admin/pages/add-course/add-course.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { AddCourseService } from './add-course.service';
+import { CourseDashboardService } from 'src/app/common/course-dashboard/course-dashboard.service';
 import { SnackbarComponent } from 'src/app/components/snackbar/snackbar.component';
 @Component({
   selector: 'app-add-course',
@@ -13,10 +14,23 @@ export class AddCourseComponent implements OnInit {
   type: string;
   constructor(
     private router: Router,
-    private addCourseService: AddCourseService
+    private addCourseService: AddCourseService,
+    private courseDashboardService: CourseDashboardService
   ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.courseDashboardService.getDepartments().subscribe(
+      (res) => {
+        console.log(res);
+        this.departments = res.data;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+  departments: any;
+
   onSubmit(form: any) {
     this.addCourseService
       .addCourse(form.course_name, form.course_code, form.dept_code)
